372655c0bbaaa279b5db339764c9b61fff6a1bf3
rksumon51 <rk.sumon51@gmail.com>
2026-09-18 23:51:51 +0600

    Uncomment manager and upload module imports

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import sys
 import subprocess
 
 from database import db_connect
-# from managers import page_manager
-# from managers import source_manager
-# from managers import mapping_manager
-# from core_upload import upload_live
-# from core_upload import upload_schedule
+from managers import page_manager
+from managers import source_manager
+from managers import mapping_manager
+from core_upload import upload_live
+from core_upload import upload_schedule
 
 DB_CONNECTED = False 
 
@@ -58,12 +58,12 @@ def main_menu():
         if choice == '1':
             if check_connection():
                 print("[*] Loading Live Upload Module...")
-                # upload_live.run()
+                upload_live.run()
                 input("Press Enter...")
         elif choice == '2':
             if check_connection():
                 print("[*] Loading Scheduled Upload Module...")
-                # upload_schedule.run()
+                upload_schedule.run()
                 input("Press Enter...")
         elif choice == '3':
             print("[*] Connecting to Firebase...")
@@ -73,19 +73,13 @@ def main_menu():
             input("Press Enter...")
         elif choice == '4':
             if check_connection():
-                print("[*] Opening Page Manager...")
-                # page_manager.menu() 
-                input("Press Enter...")
+                page_manager.menu() 
         elif choice == '5':
             if check_connection():
-                print("[*] Opening Source Manager...")
-                # source_manager.menu()
-                input("Press Enter...")
+                source_manager.menu()
         elif choice == '6':
             if check_connection():
-                print("[*] Opening Mapping Manager...")
-                # mapping_manager.menu()
-                input("Press Enter...")
+                mapping_manager.menu()
         elif choice == '7':
             update_tool()
         elif choice == '0':
